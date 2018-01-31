183298ace91f32bcf72219d7f8b37673cddb25f1
addisonkaufmann <addisonkaufmann@email.arizona.edu>
2018-01-30 22:40:30 -0700

    front end files

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ui.router', 'chart.js']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     
@@ -10,15 +10,23 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
         .state('home', {
             url: '/home',
-            templateUrl: 'templates/partial-home.html'
+            templateUrl: 'templates/partial-home.html',
+            controller: 'homeController'
         })
         
         // nested list with custom controller
         .state('home.list', {
             url: '/list',
             templateUrl: 'templates/partial-home-list.html',
-            controller: function($scope) {
+            controller: function($scope, $window) {
                 $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                $scope.width = $window.innerWidth;
+                $scope.loaded = function(){
+                    console.log("hey bitches");
+                };
+                angular.element($window).on('resize', function () {
+                    console.log($window.innerWidth);
+                });
             }
         })
         
@@ -28,41 +36,62 @@ app.config(function($stateProvider, $urlRouterProvider) {
             template: 'I could sure use a drink right now.'
         })
 
+        .state('detail', {
+            url: '/detail/:county/:contaminant', 
+            templateUrl: 'templates/detail.html', 
+            controller: 'detailController', 
+            resolve: {
+                GilaData: function($http, $stateParams){
+                    var path = "assets/data/Gila/Gila" + $stateParams.contaminant + ".json";
 
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+                    return $http.get(path).then(function onSuccess(response){
+                        return response.data;
+                    })
+                    .catch(function onError(response){
+                        return 0;
+                    });
+                },
+                YavapaiData: function($http, $stateParams){
+                    var path = "assets/data/Yavapai/Yavapai" + $stateParams.contaminant + ".json";
+
+                    return $http.get(path).then(function onSuccess(response){
+                        return response.data;
+                    })
+                    .catch(function onError(response){
+                        return 0;
+                    });
+                },
+                PimaData: function($http, $stateParams){
+                    var path = "assets/data/Pima/Pima" + $stateParams.contaminant + ".json";
+
+                    return $http.get(path).then(function onSuccess(response){
+                        return response.data;
+                    })
+                    .catch(function onError(response){
+                        return 0;
+                    });
+                },
+            }
+        })
+
+
         .state('about', {
             url: '/about',
-            views: {
-                '': { templateUrl: 'templates/partial-about.html' },
-                'columnOne@about': { template: 'Look I am a column!' },
-                'columnTwo@about': { 
-                    templateUrl: 'templates/table-data.html',
-                    controller: 'scotchController'
-                }
-            }
-            
+            templateUrl: 'templates/partial-about.html', 
+            controller: function($scope, $window, $document) {
+                $scope.showIntro = true;
+                $scope.showHistory = false;
+                $scope.showWater = false;
+                $scope.showRipple = false;
+            }          
+        })
+
+        .state('human-effect', {
+            url: '/human-effect',
+            templateUrl: 'templates/partial-human-effect.html',        
         });
         
 });
 
-app.controller('scotchController', function($scope) {
-    
-    $scope.message = 'test';
-   
-    $scope.scotches = [
-        {
-            name: 'Macallan 12',
-            price: 50
-        },
-        {
-            name: 'Chivas Regal Royal Salute',
-            price: 10000
-        },
-        {
-            name: 'Glenfiddich 1937',
-            price: 20000
-        }
-    ];
-    
-});
+
+
---
+++ www/js/controllers.js
@@ -0,0 +1,217 @@
+
+
+app.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
+});
+
+
+app.controller('detailController', function($scope, $stateParams, $state, $http, GilaData, YavapaiData, PimaData) {
+    $scope.contaminant = $stateParams.contaminant;
+    $scope.county = $stateParams.county;
+    $scope.labels = [];
+    $scope.data = [];
+    $scope.data[0] = [];
+    $scope.data[1] = [];
+    var mcl;
+    var allData = {
+        "Gila": GilaData,
+        "Yavapai": YavapaiData,
+        "Pima": PimaData
+    };
+    var data = allData[$stateParams.county];
+    console.log(data);
+
+    var allColors = {
+        "Arsenic": "#5fbcbf",
+        "Nitrate": "#266067", 
+        "Fluoride": "#337d7b", 
+        "pH": "#8bb5ad", 
+        "Lead": "#8fc495"
+    }
+    $scope.thisColor = allColors[$scope.contaminant];
+    $scope.pieColors = [$scope.thisColor, "#efefef"];
+
+    switch($stateParams.contaminant) {
+    case "Arsenic":
+        mcl = 0.01;
+        break;
+    case "Lead":
+        mcl = 0.015;
+        break;
+    case "Nitrate":
+        mcl = 10; 
+        break;
+    case "Fluoride":
+        mcl = 4;
+        break;
+    case "pH":
+        mcl = 6.5;
+        break;
+    default:
+        mcl = 0;
+        break;
+    }
+
+    $scope.mcl = mcl;
+    var exceed = 0;
+    for (var i = 0; i < data.length; i++){
+        if (data[i].Exceeds){
+            exceed++;
+        }
+        if ( $scope.labels.indexOf(data[i].Date) == -1) {
+            $scope.labels.push(data[i].Date);
+            $scope.data[1].push(data[i].Value);
+            $scope.data[0].push(mcl);
+
+        }
+     }
+    $scope.piePercent = (exceed/data.length * 100).toFixed(1);
+    $scope.pieData = [$scope.piePercent, 100 - $scope.piePercent];
+
+     $scope.colors = ['#0b193e', $scope.thisColor, '#5a5895', '#9c7fad', '#266067', '#5fbcbf'];
+
+
+
+    
+    $scope.message = $stateParams.county + " " + $stateParams.contaminant;
+    console.log($stateParams);
+
+    // $scope.labels = ["2005", "2008", "2011", "2014"];
+    $scope.series = ["Arsenic", "MCL"];
+    // $scope.data = [
+    //   [65, 59, 80, 81, 56, 55, 40]
+    // ];
+    $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+    };
+
+    $scope.datasetOverride = [ 
+        { 
+            yAxisID: 'y-axis-1', 
+            borderWidth: 2, 
+            fill: false, 
+            backgroundColor:'#0b193e'
+        },
+        { 
+            yAxisID: 'y-axis-1', 
+            borderWidth: 2, 
+            fill: true, 
+            backgroundColor:$scope.thisColor
+        },
+        { 
+            yAxisID: 'y-axis-1', 
+            borderWidth: 2, 
+            fill: true, 
+            backgroundColor:'#5a5895'
+        },
+        { 
+            yAxisID: 'y-axis-1', 
+            borderWidth: 2, 
+            fill: true, 
+            backgroundColor:'#9c7fad'
+        },
+        { 
+            yAxisID: 'y-axis-1', 
+            borderWidth: 2, 
+            fill: true, 
+            backgroundColor: '#266067'
+        },
+        { 
+            yAxisID: 'y-axis-1', 
+            borderWidth: 2, 
+            fill: true, 
+            backgroundColor:'#5fbcbf'
+        } 
+    ];
+
+      $scope.options = {
+        elements: {
+            point: { radius: 0 }
+        },
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            }
+          ]
+        }
+      };
+
+      $scope.pieOptions = {
+            tooltips : {
+                enabled: false
+            }
+      };
+
+
+    
+});
+
+app.controller('homeController', ['$scope', '$window', '$sce', '$state', '$stateParams', function($scope, $window, $sce, $state, $stateParams){
+
+
+    $scope.counties = ['Pima', 'Gila', 'Yavapai'];
+    $scope.contaminants = ['Arsenic', 'Lead', 'Nitrate', 'Fluoride', 'pH'];
+
+    $scope.width = $window.innerWidth;
+    $scope.isNarrowScreen = $window.innerWidth <= 992;
+
+
+    angular.element($window).on('resize', function () {
+        if (!$scope.isNarrowScreen && $window.innerWidth <= 992){
+            $scope.isNarrowScreen = true;
+            $scope.$apply();
+        } else if ($scope.isNarrowScreen && $window.innerWidth > 992) {
+            $scope.isNarrowScreen = false;
+            $scope.$apply();
+        }
+    });
+
+    $scope.playVideo = function(row, col){
+        // var vidid = "video" + row + col;
+        // var elem = document.getElementById(vidid);
+        // elem.play();
+    };
+
+    $scope.pauseVideo = function(row, col){
+        // var vidid = "video" +row +col;
+        // var elem = document.getElementById(vidid);
+        // elem.pause();
+    };
+
+
+    $scope.goDetail = function(row, col){
+        var county = $scope.counties[col];
+        var con = $scope.contaminants[row];
+        if (!$scope.isNarrowScreen){
+            county = $scope.counties[row];
+            con = $scope.contaminants[col];
+        }
+        console.log("going to detail page for " + county + " " + con);
+
+        $state.go('detail', {county: county, contaminant: con});
+    };
+
+}]);
+
+
